6664525b4e8be12598393392a8b13e02ba0f3385
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2017-08-23 16:53:57 -0300

    Iniciando os estudos

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<h1>{{ titulo }}</h1>
+		<ul>
+			<li v-for="foto of fotos">
+				<img :src="foto.url" :alt="foto.titulo">
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+  
+	data() {
+
+		return {
+			titulo: 'Alurapic',
+			fotos: [
+				{
+					url: 'https://i.ytimg.com/vi/WdkT0dBDuug/hqdefault.jpg',
+					titulo: 'cachorro1'
+				},
+				{
+					url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorros-fofos-08.jpg',
+					titulo: 'cachorro2'
+				}
+			]
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
