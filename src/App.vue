6eb0ce5f125d592d06f0e38e71293fa31a7587a0
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2017-08-25 16:47:51 -0300

    Aula 5. Deixando tudo mais alegre com efeitos.

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,87 @@
 <template>
-	<div>
-		<h1>{{ titulo }}</h1>
-		<ul>
-			<li v-for="foto of fotos">
-				<img :src="foto.url" :alt="foto.titulo">
+	<div class="corpo">
+		<h1 class="centralizado">{{ titulo }}</h1>
+		<input type="search" class="filtro" @input="filtro = $event.target.value" 
+			placeholder="filtre pelo título da foto">
+		<ul class="lista-fotos">
+			<li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+				<meu-painel :titulo="foto.titulo">
+					<img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+				</meu-painel>
 			</li>
 		</ul>
 	</div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+	components: {
+
+		'meu-painel': Painel
+	},
   
 	data() {
 
 		return {
 			titulo: 'Alurapic',
-			fotos: [
-				{
-					url: 'https://i.ytimg.com/vi/WdkT0dBDuug/hqdefault.jpg',
-					titulo: 'cachorro1'
-				},
-				{
-					url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorros-fofos-08.jpg',
-					titulo: 'cachorro2'
-				}
-			]
+			fotos: [],
+			filtro: ''
 		}
+	},
+
+	computed: {
+
+		fotosComFiltro() {
+
+			if(this.filtro) {
+				let exp = new RegExp(this.filtro.trim(), 'i');
+				return this.fotos.filter(foto => exp.test(foto.titulo));
+			} else {
+				return this.fotos;
+			}
+		}
+	},
+
+	created() {
+
+		let promise = this.$http.get('http://localhost:3000/v1/fotos');
+		promise
+			.then(res => res.json())
+			.then(imagens => this.fotos = imagens, err => console.log(err));
 	}
 }
 </script>
 
 <style>
+.corpo {
+	font-family: Helvetica, sans-serif;
+	width: 96%;
+	margin: 0 auto;
+}
+
+.centralizado {
+	text-align: center;
+}
+
+.lista-fotos {
+	list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+	display: inline-block;
+}
+
+.imagem-responsiva {
+	width: 100%;
+}
+
+.filtro {
+	display: block;
+	width: 50%;
+	margin: 0 auto;
+}
 
 </style>
